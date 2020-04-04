dce0b831deae9734bd79ec1c78d27044b6375e81
MAC <jookoyoski@gmail.com>
2020-04-04 22:11:18 +0100

    text

--- src/main/java/com/laundryservice/laundryservice/domain/UserRegistrationRequest.java
+++ src/main/java/com/laundryservice/laundryservice/domain/UserRegistrationRequest.java
@@ -5,7 +5,7 @@ public class UserRegistrationRequest extends User{
     public String getConfirmPassword() {
         return confirmPassword;
     }
-
+//confirm password
     public void setConfirmPassword(String confirmPassword) {
         this.confirmPassword = confirmPassword;
     }
